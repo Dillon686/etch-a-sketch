6177b39778d4f22f669798827cf0d8fdfd510526
Dillon Richardson <dillonrichardson6@gmail.com>
2024-12-31 21:50:42 -0500

    Completed hover functionality, created button to resize grid

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const gameContainer = document.querySelector("#game-container");
+const sizeButton = document.querySelector("#size-button");
+let gridSize = 16;
 
-for (let i = 0; i < 16; i++){
-    let row = document.createElement("div");
+drawGrid(gridSize);
 
-    for (let j = 0; j < 16; j++){
-        let cell = document.createElement("div");
-        row.appendChild(cell);
+function drawGrid(gridSize){
+    gameContainer.textContent = "";
+
+    for (let i = 0; i < gridSize; i++){
+        let row = document.createElement("div");
+    
+        for (let j = 0; j < gridSize; j++){
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+    
+        gameContainer.appendChild(row)
     }
 
-    gameContainer.appendChild(row)
+    let gridCells = document.querySelectorAll(".cell");
+
+    gridCells.forEach((cell) => {
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "black";
+    })
+})
 }
+
+sizeButton.addEventListener("click", () => {
+    gridSize = prompt("Please select a grid size (value must be between 2-100):")
+
+    while (gridSize <= 1 || gridSize > 100){
+        gridSize = prompt("Please select a grid size (value must be between 2-100):")
+    }
+
+    drawGrid(gridSize);
+})
